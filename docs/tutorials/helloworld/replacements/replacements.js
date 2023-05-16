ba90b1f060479e894595a5c8040293e6d5b82c98
Carsten Munk <carsten@zippie.com>
2023-05-16 09:05:27 +0200

    Update replacements
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- docs/tutorials/helloworld/replacements/replacements.js
+++ docs/tutorials/helloworld/replacements/replacements.js
@@ -2,7 +2,7 @@ module.exports.run = async function() {
     await generateOutput(__dirname, "tutorials.helloworld.run");
     const store = await generateOutput(__dirname, "tutorials.helloworld.store");
     const hash = findHash(store, "0");
-    await generateOutput(__dirname, "tutorials.helloworld.storedir");
+    await generateOutput(__dirname, "tutorials.helloworld.dir");
     replacements["tutorials.helloworld.hash-full"] = hash;
     replacements["tutorials.helloworld.hash-trunc"] = truncateHash(hash);
 }
