c2a30fdbe46fe0c6cbbacc9294754e95b7cc64cc
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2023-08-03 16:09:05 +0300

    Tagline & Thumbnail Image update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const replacementPlugin = require("./src/remark/replacement");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Cartesi Documentation",
-  tagline: "Cartesi",
+  tagline: "Application-specific rollups with a Linux runtime.",
   url: "https://docs.cartesi.io",
   baseUrl: "/",
   trailingSlash: true,
@@ -31,7 +31,6 @@ const config = {
           remarkPlugins: [replacementPlugin],
           docLayoutComponent: "@theme/DocPage",
           docItemComponent: "@theme/ApiItem",
-          
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -188,7 +187,6 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ["lua"],
       },
-
 
       /*  // Announcemnt bar
       announcementBar: {
