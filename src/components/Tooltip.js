6a7e77cbca2f16b401e0c1ad4e217bd8f94dde2e
Joro <joro.georgiev@gmail.com>
2023-09-18 12:07:31 +0300

    Tooltip fix

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,12 +2,25 @@ import React from "react";
 import { Tooltip as _Tooltip } from "react-tooltip";
 
 export default function Tooltip({ children, content }) {
+  const id = content.replace(/\s+/g, "-").toLowerCase();
   return (
     <>
-      <span data-tooltip-id="tooltip" data-tooltip-content={content}>
+      <span
+        data-tooltip-id={id}
+        data-tooltip-content={content}
+        style={{
+          borderBottom: "1px dotted var(--ifm-font-color-base)",
+        }}
+      >
         {children}
       </span>
-      <_Tooltip id="tooltip" />
+      <_Tooltip
+        id={id}
+        style={{
+          fontSize: ".875rem",
+          maxWidth: "20rem",
+        }}
+      />
     </>
   );
 }
