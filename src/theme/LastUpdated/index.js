b6ddabe28043bffb5d2cbfecc42644012e2b216d
Joro <joro.georgiev@gmail.com>
2024-02-29 10:58:47 +0200

    Svg fixes

--- src/theme/LastUpdated/index.js
+++ src/theme/LastUpdated/index.js
@@ -43,17 +43,17 @@ export default function LastUpdated({
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="w-4 h-4 fill-current shrink-0"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path
           d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 2.66a1 1 0 0 0 -.993 .883l-.007 .117v5l.009 .131a1 1 0 0 0 .197 .477l.087 .1l3 3l.094 .082a1 1 0 0 0 1.226 0l.094 -.083l.083 -.094a1 1 0 0 0 0 -1.226l-.083 -.094l-2.707 -2.708v-4.585l-.007 -.117a1 1 0 0 0 -.993 -.883z"
-          stroke-width="0"
+          strokeWidth="0"
           fill="currentColor"
         />
       </svg>
