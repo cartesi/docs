91ae2c87d4b1010ede28e418174296b97899c59c
Joro <joro.georgiev@gmail.com>
2024-03-27 16:12:22 +0200

    Redirect to home fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 function Home() {
   // return <Redirect to="/cartesi-rollups" />;
-  return <Redirect to="/cartesi-rollups/1.3" />;
+  return <Redirect to="/cartesi-rollups/1.3/" />;
 }
 
 export default Home;
