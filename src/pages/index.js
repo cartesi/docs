1e3056f9702b84d9deea56966fc80839817e253b
Joro <joro.georgiev@gmail.com>
2024-03-27 16:42:57 +0200

    Redirect to home fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect } from "@docusaurus/router";
+
 function Home() {
   // return <Redirect to="/cartesi-rollups" />;
   return <Redirect to="/cartesi-rollups/1.3/" />;
