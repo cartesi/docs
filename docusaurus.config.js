5eef223fc35514a995ce7d4b9eabf3535f38c268
Michael Asiedu <michaelasiedu148@gmail.com>
2024-04-15 15:52:39 +0530

    update banner for honeypot

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -220,7 +220,7 @@ const config = {
       announcementBar: {
         id: "mainnet",
         content:
-          'Cartesi Rollups is Mainnet Ready! Over 200K in CTSI is up for grabs... if you can <a href="https://docs-azure-two.vercel.app/cartesi-rollups/core-concepts/mainnet-considerations/" target="_blank" rel="noopener noreferrer">hack Cartesi Rollups</a>.',
+          'Cartesi Rollups is Mainnet Ready! Over 300K in CTSI is up for grabs... if you can <a href="https://honeypot.cartesi.io/" target="_blank" rel="noopener noreferrer">hack Cartesi Rollups</a>.',
 
         backgroundColor: "rgba(0, 0, 0, 0.7)",
         textColor: "#FFFFFF",
