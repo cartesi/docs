dc648fb2559697586f0324f4dfa1d4beaa6e53b7
Joro <joro.georgiev@gmail.com>
2024-12-09 17:24:40 +0200

    edit this page, custom icons

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Translate from "@docusaurus/Translate";
 import { ThemeClassNames } from "@docusaurus/theme-common";
+import Link from "@docusaurus/Link";
 import IconEdit from "@theme/Icon/Edit";
 export default function EditThisPage({ editUrl }) {
   return (
