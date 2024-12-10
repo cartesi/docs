dc28603fcb7c57046ae18eb221f536228f354726
Joro <joro.georgiev@gmail.com>
2024-12-10 09:44:18 +0200

    tag

--- src/theme/Tag/index.js
+++ src/theme/Tag/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
-export default function Tag({permalink, label, count}) {
+export default function Tag({permalink, label, count, description}) {
   return (
     <Link
       href={permalink}
+      title={description}
       className={clsx(
         styles.tag,
         count ? styles.tagWithCount : styles.tagRegular,
