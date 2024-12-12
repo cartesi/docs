df119ed79797593809ab858420101c528fd2cea0
Joro <joro.georgiev@gmail.com>
2024-12-12 15:03:12 +0200

    toc
    (cherry picked from commit aab9ec8b9116f1f51ac5ea4fced5381084215a9b)

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import clsx from "clsx";
 import TOCItems from "@theme/TOCItems";
 import styles from "./styles.module.css";
-import { useDoc } from "@docusaurus/theme-common/internal";
+import Resources from "@site/src/components/Resources";
+
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
 // Using a custom className
 // This prevents TOCInline/TOCCollapsible getting highlighted by mistake
 const LINK_CLASS_NAME = "table-of-contents__link toc-highlight";
 const LINK_ACTIVE_CLASS_NAME = "table-of-contents__link--active";
-import Resources from "../../components/Resources";
-
 export default function TOC({ className, ...props }) {
-  const { frontMatter } = useDoc();
+  const { toc, frontMatter } = useDoc();
   return (
     <div className={clsx(styles.tableOfContents, "thin-scrollbar", className)}>
       <h3 className="mt-4 block text-[.8rem]">On this page</h3>
