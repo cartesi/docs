10e850196812549a5ff118d13cef6f4b4f8f8d1e
Shaheen <riseandshaheen@gmail.com>
2025-08-25 17:26:23 +0530

    Fraud Proofs Tab- Content
    (cherry picked from commit c9c385f1eb7ce29d78e9277d909b248af6e6106d)

---
+++ sidebarsFraudProofs.js
@@ -0,0 +1,107 @@
+module.exports = {
+  daveSidebar: [
+    {
+      type: 'doc',
+      id: 'index', 
+      label: 'Overview',
+    },
+    {
+      type: 'category',
+      label: 'Fraud Proof Basics',
+      collapsed: false,
+      items: [
+        {
+          type: 'doc',
+          id: 'fraud-proof-basics/introduction',
+          label: 'Introduction',
+        },
+        {
+          type: 'doc',
+          id: 'fraud-proof-basics/state-transition-function',
+          label: 'State Transition Function',
+        },
+        {
+          type: 'doc',
+          id: 'fraud-proof-basics/epochs',
+          label: 'Epoch Lifecycle',
+        },
+        {
+          type: 'doc',
+          id: 'fraud-proof-basics/bonds',
+          label: 'Bonds',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Permissionless Refereed Tournament (PRT)',
+      collapsed: false,
+      items: [
+        {
+          type: 'doc',
+          id: 'prt/prt-introduction',
+          label: 'Introduction',
+        },
+        {
+          type: 'doc',
+          id: 'prt/prt-algorithm',
+          label: 'Algorithm',
+        },
+        {
+          type: 'doc',
+          id: 'prt/prt-architecture',
+          label: 'Architecture',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Honeypot Application',
+      collapsed: false,
+      items: [
+        {
+          type: 'doc',
+          id: 'honeypot/introduction',
+          label: 'Introduction',
+        },
+        {
+          type: 'doc',
+          id: 'honeypot/application-logic',
+          label: 'Application logic',
+        },
+        {
+          type: 'doc',
+          id: 'honeypot/prt-integration',
+          label: 'PRT Integration',
+        }
+      ],
+    },
+    {
+      type: 'category',
+      label: 'References',
+      collapsed: false,
+      items: [
+        {
+          type: 'doc',
+          id: 'references/daveconsensus',
+          label: 'DaveConsensus',
+        },
+        {
+          type: 'doc',
+          id: 'references/tournament',
+          label: 'Tournament',
+        },
+        {
+          type: 'doc',
+          id: 'references/deployments',
+          label: 'Deployments',
+        },
+        {
+          type: 'link',
+          label: 'GitHub',
+          href: 'https://github.com/cartesi/dave',
+        },
+      ],
+    },
+  ],
+}; 
